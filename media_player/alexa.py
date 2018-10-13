e519e17cb37d3f2d6d472e4783872e8799042749
justin144 <justin_144@msn.com>
2018-10-12 20:11:29 -0500

    Changed the blank track again

--- media_player/alexa.py
+++ media_player/alexa.py
@@ -504,7 +504,7 @@ class AlexaClient(MediaPlayerDevice):
         if not (self.state in [STATE_PLAYING, STATE_PAUSED]):
             if not (self.available):
                 return
-            self.play_media("AMAZON_MUSIC", "Silence 5 Seconds")
+            self.play_media("AMAZON_MUSIC", "Blank Track 2 (Hidden Track)")
             count = 1
             while not (self.state in [STATE_PLAYING]):
                 time.sleep(.5)
